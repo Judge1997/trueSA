2405d0a8b5a5290b9ffad2da47c98b4ad2b7d037
DudeInTheWood <kanticha.y@ku.th>
2017-10-15 14:07:48 +0700

    add database

--- src/main/java/controllers/AddRecordPageController.java
+++ src/main/java/controllers/AddRecordPageController.java
@@ -3,13 +3,16 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AddRecordPageController {
 
     @FXML
-    private TextField nameField,aeNameField,regionField,locationField,businessIDField;
+    private TextField nameField,aeNameField,regionField,businessIDField;
+    @FXML
+    private TextArea locationArea;
 
     @FXML
     public void cancelBtn(ActionEvent event){
