2d70db22425403072322a279f3f965793c6a3158
DudeInTheWood <kanticha.y@ku.th>
2017-12-21 14:04:30 +0700

    add image

--- src/main/java/controllers/StartProcController.java
+++ src/main/java/controllers/StartProcController.java
@@ -1,12 +1,18 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class StartProcController {
 
@@ -18,5 +24,28 @@ public class StartProcController {
     public void initialize() {
         Image logoTrue = new Image("trueSALoGo.png");
         logoView.setImage(logoTrue);
+    }
+
+    @FXML
+    public void customerBtn() throws IOException {
+        this.goToLogIn(1);
+    }
+
+    @FXML
+    public void packageBtn() throws IOException {
+        this.goToLogIn(2);
+    }
+
+    @FXML
+    public void goToLogIn(int Emtype) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginPage.fxml"));
+        LoginPageController loginPageController = new LoginPageController();
+        Stage stage = new Stage();
+        Parent root = loader.load();
+        stage.setTitle("TrueSA");
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setResizable(false);
+        stage.show();
+
     }
 }
