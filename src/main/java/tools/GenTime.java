05193ae1c96bac9373b64fb36d389a055d197718
judge1997 <chalermpol.ki@ku.th>
2017-12-28 10:36:28 +0700

    update

--- src/main/java/tools/GenTime.java
+++ src/main/java/tools/GenTime.java
@@ -6,23 +6,23 @@ import java.util.Date;
 public class GenTime {
 
     public String getTime(){
-        SimpleDateFormat formatDate = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
         String genDate = formatDate.format(new Date());
 
         return genDate;
     }
 
     public String getTimePlusOneYear(String date){
-        int day = Integer.parseInt(date.substring(0,2));
-        int month = Integer.parseInt(date.substring(3,5));
-        int year = Integer.parseInt(date.substring(6,10));
+        int day = Integer.parseInt(date.substring(8,10));
+        int month = Integer.parseInt(date.substring(5,7));
+        int year = Integer.parseInt(date.substring(0,4));
 
         if (month == 2 && day == 29){
             day = 28;
         }
 
         year++;
-        String newDate = String.format("%02d-%02d-%04d",day,month,year);
+        String newDate = String.format("%04d-%02d-%02d",year,month,day);
 
         return newDate;
     }
